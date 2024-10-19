1aeff9c46dea840d05ed47f1c1f599e0d36e3670
Aurobindo Tripathy <aurobindotripathy@Aurobindos-MacBook-Pro.local>
2024-10-18 21:30:11 -0700

    folder

---
+++ query-your-dish-washer-repair-man/README.md
@@ -0,0 +1,9 @@
+# Query your Dish-washer Repair Manual
+
+Writeup on Medium over [here](https://medium.com/@auro-227/gpt3-does-dishes-no-use-it-to-query-your-dishwasher-repair-manual-39af99e017a0).
+
+Data is scraped from a [dish repair mannual found on the web](https://www.appliancerepair.net/dishwasher-repair-1.html).
+
+Copyright note for the content: Copyright © 2005 - EB Publishing Inc. All Rights Reserved.
+
+Finally, a comparision of two search method methods: (1) naive nearest-neightbour search vs (2) hnswlib-based approximate nearest-neightbour (ANN) search (in log(n) time) at [ANN-with-hnswlib notebook](./hnswlib-app/ANN-with-hnswlib.ipynb)
---
+++ query-your-dish-washer-repair-man/scraper.py
@@ -0,0 +1,65 @@
+"""
+This scrapes the text from several URLs and organizes them 
+for creating embeddings. Its not totally layout agnostic
+but it beats manual scraping
+"""
+# bs4 hacking ideas from https://stackoverflow.com/questions/34589064/beautifulsoup-get-the-class-text
+
+import requests
+from bs4 import BeautifulSoup, NavigableString, Tag
+import csv
+import re
+from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+
+max_chunk_size = 70  # arbitrary for now
+
+def gen_text_for_embeddings(url, f, writer, total, title):
+    """total is the sentence count up to this point"""
+    
+    chuck_size = 0
+    chunk = ''
+    html_text = requests.get(url).text
+    
+    soup = BeautifulSoup(html_text, 'html.parser')
+    merge_text = None
+    for i, para in enumerate(soup.findAll('p')):
+        # writer.writerow([f'***{i}***'])
+        text = ''
+        if para.find_all('strong'):
+            merge_text = para.text
+            continue
+        if merge_text is not None:
+            text = '' + merge_text + ': '
+            merge_text = None
+    
+        for child in para:
+            
+            if isinstance(child, NavigableString):
+                text += str(child).strip()
+                text = re.sub(r"(\231|\200|\204)", "", text)  # cleanup
+            elif isinstance(child, Tag):
+                if child.name != 'br' or child.name != 'em':
+                    text = text + ' ' + child.text + ' '
+                else:
+                    text += '\n'
+        chunk += ' ' + text
+        tokenizer = RegexpTokenizer(r'\w+')
+        if len(tokenizer.tokenize(chunk)) > max_chunk_size:
+            tokens = tokenizer.tokenize(chunk)
+            writer.writerow([title, f'{str(total + i)}', chunk, len(tokens)])
+            chunk = ''
+
+    return total + len(soup.find_all('p')) 
+
+# main
+out_file = 'dish-washer-data.csv'   
+f_handle = open(out_file, 'w')
+writer = csv.writer(f_handle, quoting=csv.QUOTE_ALL)
+writer.writerow(["title", "heading", "content", "tokens"])
+total_sentences = 0
+for i in range(1, 7):
+    title = f'Chapter_{i}'
+    url = f'https://www.appliancerepair.net/dishwasher-repair-{i}.html'
+    total_sentences = gen_text_for_embeddings(url, f_handle, writer, total_sentences, title)   
+f_handle.close()
